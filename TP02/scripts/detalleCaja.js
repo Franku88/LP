e44899c835f8371c29133f7de6ca8dfa46b9373a
jamiroZ <jamiro.zuniga@est.fi.uncoma.edu.ar>
2025-10-10 18:09:12 -0300

    agregue la funcionalidad de que al precionar una caja de la tienda muestre el contenido y al comprar alguna caja que la agrege al inventario

--- TP02/scripts/detalleCaja.js
+++ TP02/scripts/detalleCaja.js
@@ -22,7 +22,7 @@ if (!window.__crateViewerLoaded) {
         throw new Error(`La caja "${crate.name}" no tiene la propiedad "contains"`);
       }
 
-      // Crear estructura del detalle
+      // Crear estructura del detalle de la cajas y las skins
       const caseDiv = document.createElement("div");
       caseDiv.classList.add("case");
       const cajaImgHTML = crate.image
@@ -31,17 +31,59 @@ if (!window.__crateViewerLoaded) {
 
       caseDiv.innerHTML = `
         <h1>${crate.name}</h1>
-        ${cajaImgHTML}
-        <p><strong>ID:</strong> ${crate.id}</p>
-        <p><strong>Primera venta:</strong> ${crate.first_sale_date || "Desconocida"}</p>
+        <div class="crate-info">
+            <div class="crate-image-wrapper" data-id="${crate.id}">
+                <img src="${crate.image}" alt="${crate.name}" class="crate-image">
+                <span class="tooltip">ID: ${crate.id}</span>
+            </div>
+            <div class="crate-actions">
+              <button class="btn-comprar" data-crateid="${crate.id}">comprar caja</button>
+            </div>
+        </div>
         <h2>Contiene:</h2>
         <div class="skins"></div>
         ${crate.contains_rare && crate.contains_rare.length > 0 ? '<h2>Skins raras:</h2><div class="skins rare"></div>' : ''}
       `;
+      //evento para comprar la caja
+      const btnComprar= caseDiv.querySelector(".btn-comprar");
+      btnComprar.addEventListener("click",() => comprarCaja(crate));
+      
+      /*function actualizarInventarioUI(){
+        const inventario=JSON.parse(localStorage.getItem("inventarioCajas")) ||[];
+        const info= document.getElementById("inventario-info");
+        if(info){
+          info.textContent=`Tenes ${inventario.length} caja(s) en tu inventario.`;
+        }
+      }*/
+      function comprarCaja(crate){
+        //busco el inventario actual del almacenamiento local
+        //localStorage: es un almacenamiento del navegador,guarda info (txt plano)
+        
+        //parse conviete de txt JSON en objeto
+        const inventario= JSON.parse(localStorage.getItem("inventarioCajas")) ||[];
+        //creo un objeto con datos de la caja comprada
+        const nuevaCaja ={
+          id:crate.id,
+          name: crate.name,
+          image: crate.image,
+          fecha: new Date().toLocaleString()
+        };
+        //agrego al inventario
+        inventario.push(nuevaCaja);
+        //guarda el inventario actualizado
+        //stringify transforma objeto en texto JSON
+        localStorage.setItem("inventarioCajas",JSON.stringify(inventario));
+        //muestra confirmacion
+        alert(`Has comprado la caja "${crate.name}"`);
+        //
+        window.location.href="../pages/inventario.html";
+      }
+
 
       const skinsDiv = caseDiv.querySelector(".skins");
       const rareDiv = caseDiv.querySelector(".skins.rare");
-
+      //comprar caja
+  
       // Skins normales
       crate.contains.forEach(item => {
         const skinData = allSkins.find(s => s.id === item.id);
@@ -51,8 +93,6 @@ if (!window.__crateViewerLoaded) {
           skinDiv.innerHTML = `
             <img src="${skinData.image}" alt="${skinData.name}">
             <h3>${skinData.name}</h3>
-            <p><strong>ID:</strong> ${skinData.id}</p>
-            <p><strong>Rareza:</strong> ${skinData.rarity || "Desconocida"}</p>
           `;
           skinsDiv.appendChild(skinDiv);
         }
@@ -68,8 +108,6 @@ if (!window.__crateViewerLoaded) {
             skinDiv.innerHTML = `
               <img src="${skinData.image}" alt="${skinData.name}">
               <h3>${skinData.name}</h3>
-              <p><strong>ID:</strong> ${skinData.id}</p>
-              <p><strong>Rareza:</strong> ${skinData.rarity || "Desconocida"}</p>
             `;
             rareDiv.appendChild(skinDiv);
           }
--- TP02/scripts/scripts.js
+++ TP02/scripts/scripts.js
@@ -143,5 +143,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     })
     .catch(err => console.error("Error cargando cajas:", err));
-
+    
+    //carga las cajas compradas al inventario
+    const container =document.getElementById("inventario-container");
+    const inventario=JSON.parse(localStorage.getItem("inventarioCajas"))|| [];
+    
+    if(inventario.length === 0){//no hay cajas en el inventario
+       container.innerHTML="<p> inventario vacio</p>";
+       return;
+    }
+    container.innerHTML=inventario.map(caja =>`
+      <div class="item-caja">
+         <img src="${caja.image}" alt="${caja.name}" class="img-caja">
+         <h3>${caja.name}</h3>
+         <p>ID: ${caja.id}</p>
+         <p>Comprada el ${caja.fecha}</p>
+      </div>`),join("");
 });
